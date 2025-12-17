6a267f056515810fea43854d730127cf1c11b50e
Waldo <117220202+waldiiiFd@users.noreply.github.com>
2025-12-17 17:32:49 -0500

    fix:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,47 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     define: {
-      'import.meta.env.PUBLIC_EMAILJS_SERVICE_ID': JSON.stringify(process.env.PUBLIC_EMAILJS_SERVICE_ID),
-      'import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID': JSON.stringify(process.env.PUBLIC_EMAILJS_TEMPLATE_ID),
-      'import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY': JSON.stringify(process.env.PUBLIC_EMAILJS_PUBLIC_KEY),
-      'import.meta.env.PUBLIC_RECIPIENT_EMAIL': JSON.stringify(process.env.PUBLIC_RECIPIENT_EMAIL),
-    }
-  },
-
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-    routing:{
-      prefixDefaultLocale: true,
-    }
-  },
-  site: 'https://waldofernandezdev.netlify.app',
-  integrations: [sitemap({
-  i18n: {
-    defaultLocale: 'en',
-    locales: {
-      en: 'en',
-      es: 'es',
+      "import.meta.env.PUBLIC_EMAILJS_SERVICE_ID": JSON.stringify(
+        process.env.PUBLIC_EMAILJS_SERVICE_ID
+      ),
+      "import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID": JSON.stringify(
+        process.env.PUBLIC_EMAILJS_TEMPLATE_ID
+      ),
+      "import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY": JSON.stringify(
+        process.env.PUBLIC_EMAILJS_PUBLIC_KEY
+      ),
+      "import.meta.env.PUBLIC_RECIPIENT_EMAIL": JSON.stringify(
+        process.env.PUBLIC_RECIPIENT_EMAIL
+      ),
     },
   },
-})],
 
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+  site: "https://waldofernandezdev.netlify.app",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          es: "es",
+        },
+      },
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 });
